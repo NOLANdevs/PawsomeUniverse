c1c88659fa8ec15328fb1c89463243c1ca11a5a8
nate <jmy3092@autuni.ac.nz>
2023-08-24 17:39:45 +1200

    1D player movement with ground

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,18 +6,33 @@ public class Movement : MonoBehaviour
 {
 
     public float moveSpeed = 1f;
+    public float jumpForce = 1f;
+
+    private Rigidbody2D rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
-        // get user input
-        float horiz = Input.GetAxis("Horizontal");
-        float vert = Input.GetAxis("Vertical");
+        float x = Input.GetAxisRaw("Horizontal");
+        rigidBody.velocity = new Vector2(x * moveSpeed, rigidBody.velocity.y);
 
-        // calculate movement vector
-        Vector3 moveDirection = new Vector3(horiz, vert, 0f);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
 
-        // move character
-        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+    private void Jump()
+    {
+        const float almostZero = 0.1f;
+        if (Mathf.Abs(rigidBody.velocity.y) < almostZero)
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+        }
     }
 
 }
