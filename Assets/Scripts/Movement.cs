68918e7f19edc7ddb632a0a7f07bb45621469410
StoneFreEE <79116169+StoneFreEE@users.noreply.github.com>
2023-08-24 21:09:13 +1200

    Added sprites, animations, and background
    - imported 2D sprite editor packages
    - Press 'E' to trigger eat animation
    - added ground material
    -  linked walking with animation
    - added wings, mushroom, shower, top hat

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,21 +10,69 @@ public class Movement : MonoBehaviour
 
     private Rigidbody2D rigidBody;
 
+    private Animator animator;
+    public GameObject frogAnimatorHolder;
+
+    private bool isEating = false;
+
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        animator = frogAnimatorHolder.GetComponent<Animator>(); // Get the animator from the frogAnimatorHolder
+
     }
 
     void Update()
     {
         float x = Input.GetAxisRaw("Horizontal");
+        Vector2 moveDirection = new Vector2(x, 0f); // Store move direction for idle check
         rigidBody.velocity = new Vector2(x * moveSpeed, rigidBody.velocity.y);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
         }
+
+        checkEat();
+        checkIdle(moveDirection);
+
     }
+
+    private void checkEat()
+    {
+        // press e to eat
+        if (Input.GetKey(KeyCode.E))
+        {
+            isEating = true;
+        }
+        else
+        {
+            isEating = false;
+        }
+
+        animator.SetBool("IsEating", isEating);
+
+        // while frog is eating it cannot move horizontally
+        if (isEating)
+        {
+            animator.SetBool("IsEating", true);
+            rigidBody.velocity = new Vector2(0f, rigidBody.velocity.y); // Maintain vertical velocity
+            return;
+        }
+    }
+    private void checkIdle(Vector2 moveDirection)
+    {
+        // Check if the frog is not moving
+        if (moveDirection.magnitude <= 0.1f)
+        {
+            animator.SetBool("IsIdle", true);
+        }
+        else
+        {
+            animator.SetBool("IsIdle", false);
+        }
+    }
+
 
     private void Jump()
     {
@@ -34,5 +82,4 @@ public class Movement : MonoBehaviour
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
         }
     }
-
 }
