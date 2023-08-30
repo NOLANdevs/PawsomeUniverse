ec077f08a3ee7e8b424705b304a6653c4fa9d528
nate <jmy3092@autuni.ac.nz>
2023-08-30 21:34:51 +1200

    Create sync-dev-branches.sh
    Syncs main into each dev branch to keep them all up to date

---
+++ sync-dev-branches.sh
@@ -0,0 +1,17 @@
+branches=("dev-Nicha" "dev-Oliver" "dev-Lily" "dev-Alvina" "dev-Nate")
+
+git checkout main &&
+git pull origin main
+
+for branch in "${branches[@]}"; do
+    git checkout "$branch" &&
+    git pull &&
+    if git merge main; then
+        git add . &&
+        git push
+    else
+        echo "Merge into $branch failed. Continuing with the next branch."
+    fi
+    git add . &&
+    git push
+done
