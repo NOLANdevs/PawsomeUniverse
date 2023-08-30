47bf655013b5d0a3a7d4acb3fb423817e89825e2
nate <jmy3092@autuni.ac.nz>
2023-08-30 23:49:13 +1200

    make camera zoom based on distance

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,11 +7,19 @@ public class CameraFollow : MonoBehaviour
 
     public GameObject characterToFollow;
     public float followSpeed = 5;
-    public float minY = float.NegativeInfinity;
-    public float maxY = float.PositiveInfinity;
+    public float zoomPower = 1;
+    public Bounds yBounds = new Bounds(float.NegativeInfinity, float.PositiveInfinity);
+    public Bounds zoomBounds = new Bounds(1, 5);
+
+    new private Camera camera;
+    private Vector3 center;
+    private float baseZoom;
 
     void Start()
     {
+        camera = GetComponent<Camera>();
+        baseZoom = camera.orthographicSize;
+        center = characterToFollow.transform.position; // set center to player's spawn point
     }
 
     void Update()
@@ -19,9 +27,29 @@ public class CameraFollow : MonoBehaviour
         Vector3 playerPos = characterToFollow.transform.position;
         Vector3 camPos = transform.position;
 
-        // Interpolate camera position onto player position and move camera to follow player
+        // Interpolate camera position to follow player position
         Vector3 newPos = Vector3.Lerp(camPos, playerPos, followSpeed * Time.deltaTime);
-        newPos.y = Mathf.Clamp(newPos.y, minY, maxY); // clamp to be between min and max Y
-        transform.position = new Vector3(newPos.x, newPos.y, camPos.z);
+        // Clamp Y position to be between min and max Y
+        newPos.y = Mathf.Clamp(newPos.y, yBounds.min, yBounds.max);
+        // Zoom out camera when away from center
+        float distFromCenter = Vector3.Distance(playerPos, center);
+        float zoomAmount = Mathf.Pow(distFromCenter, zoomPower);
+        Debug.Log(zoomAmount);
+        camera.orthographicSize = Mathf.Clamp(zoomAmount, zoomBounds.min, zoomBounds.max);
+        // move camera
+        newPos.z = camPos.z;
+        transform.position = newPos;
+    }
+}
+
+[System.Serializable] // This attribute allows the struct to be shown in the Inspector
+public struct Bounds
+{
+    public float min, max;
+
+    public Bounds(float min, float max)
+    {
+        this.min = min;
+        this.max = max;
     }
 }
