f5fb6f31e5fe468bd4c28266c62d5ebe4c788b67
nate <jmy3092@autuni.ac.nz>
2023-08-31 14:46:12 +1200

    Zoom camera to enview interacted object

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,22 +8,31 @@ public class CameraFollow : MonoBehaviour
     public GameObject characterToFollow;
     public float followSpeed = 5;
     public float zoomPower = 1;
+    public float lingerTime = 2;
     public Bounds yBounds = new Bounds(float.NegativeInfinity, float.PositiveInfinity);
     public Bounds zoomBounds = new Bounds(1, 5);
 
     new private Camera camera;
     private Vector3 center;
     private float baseZoom;
+    private float timeSinceLastInteraction;
 
     void Start()
     {
         camera = GetComponent<Camera>();
         baseZoom = camera.orthographicSize;
         center = characterToFollow.transform.position; // set center to player's spawn point
+        timeSinceLastInteraction = lingerTime;
     }
 
     void Update()
     {
+        timeSinceLastInteraction += Time.deltaTime;
+        if (Input.GetMouseButtonDown(0))
+        {
+            timeSinceLastInteraction = 0f;
+        }
+
         Vector3 playerPos = characterToFollow.transform.position;
         Vector3 camPos = transform.position;
 
@@ -31,18 +40,28 @@ public class CameraFollow : MonoBehaviour
         Vector3 newPos = Vector3.Lerp(camPos, playerPos, followSpeed * Time.deltaTime);
         // Clamp Y position to be between min and max Y
         newPos.y = Mathf.Clamp(newPos.y, yBounds.min, yBounds.max);
+        // Move camera
+        transform.position = new Vector3(newPos.x, newPos.y, camPos.z);
+
         // Zoom out camera when away from center
         float distFromCenter = Vector3.Distance(playerPos, center);
         float zoomAmount = Mathf.Pow(distFromCenter, zoomPower);
-        Debug.Log(zoomAmount);
         camera.orthographicSize = Mathf.Clamp(zoomAmount, zoomBounds.min, zoomBounds.max);
-        // move camera
-        newPos.z = camPos.z;
-        transform.position = newPos;
+
+        // Move camera to include interactable object for a period of time when it has been recently clicked
+        if (timeSinceLastInteraction < lingerTime)
+        {
+            Vector3 clickPosition = camera.ScreenToWorldPoint(Input.mousePosition);
+            clickPosition.z = 0;
+            float distanceToClick = Vector3.Distance(camPos, clickPosition);
+            float newOrthoSize = distanceToClick * Mathf.Tan(camera.fieldOfView * 0.5f * Mathf.Deg2Rad);
+            newOrthoSize = Mathf.Clamp(newOrthoSize, zoomBounds.min, zoomBounds.max);
+            camera.orthographicSize = newOrthoSize;
+        }
     }
 }
 
-[System.Serializable] // This attribute allows the struct to be shown in the Inspector
+[System.Serializable] // displayable in inspector
 public struct Bounds
 {
     public float min, max;
