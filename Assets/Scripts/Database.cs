54638cf4212665c32f9e433ee52f083106d3a5e4
nate <jmy3092@autuni.ac.nz>
2023-08-31 18:20:09 +1200

    Set up basic database

--- Assets/Scripts/Database.cs
+++ Assets/Scripts/Database.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class Database
+public class Database : MonoBehaviour
 {
 
-    private string dbFile;
+    public Animal animal; // testing
+    public string dbFileName = "database.db";
+
+    private string dbFilePath = "./Database/";
+    private string fullPath;
 
     void Start()
     {
@@ -14,14 +19,28 @@ public class Database
 
     public void init()
     {
+        fullPath = Path.Combine(dbFilePath, dbFileName);
+        if (!Directory.Exists(dbFilePath))
+        {
+            Directory.CreateDirectory(dbFilePath);
+        }
+
+        write("");
+        save(animal); // testing
     }
 
     public void save(Animal animal)
     {
+        write($"{animal.id},{animal.animalName},{animal.species},{animal.colour},{animal.love},{animal.hunger},{animal.cleanliness}\n");
     }
 
     public void load(int id)
     {
+    }
+
+    private void write(string data)
+    {
+        File.WriteAllText(fullPath, data);
     }
 
 }
