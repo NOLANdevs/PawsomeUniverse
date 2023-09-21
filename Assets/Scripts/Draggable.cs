48de339f052c5ef6712dbbbfebc3ed3429593d54
nate <jmy3092@autuni.ac.nz>
2023-09-21 16:53:50 +1200

    Abstractify draggable functionality

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+    private bool isDragging = false;
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        isDragging = true;
+        offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void OnMouseDrag()
+    {
+        if (isDragging)
+        {
+            Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+            transform.position = new Vector3(newPosition.x, newPosition.y, 0f);
+        }
+    }
+
+    void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+}
