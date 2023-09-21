5fca4405a81d13acf4760d7a1220baab47332cb2
nate <jmy3092@autuni.ac.nz>
2023-09-21 15:49:52 +1200

    Fix crash when trying to load money

--- Assets/Scripts/Database/DatabaseInterface.cs
+++ Assets/Scripts/Database/DatabaseInterface.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -130,7 +131,14 @@ public class DatabaseInterface : MonoBehaviour
 
     private void loadStats()
     {
-        PlayerPrefs.SetInt("Coins", int.Parse(statsDB.Read()));
+        try
+        {
+            PlayerPrefs.SetInt("Coins", int.Parse(statsDB.Read()));
+        }
+        catch (FormatException)
+        {
+            // leave Coins undefined
+        }
     }
 
 }
