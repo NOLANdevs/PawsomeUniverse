5f6d57765c0541f1fe81be136afc02b5b96fdc35
vina <alvina.angelin.s@gmail.com>
2023-10-10 20:04:38 +1300

    Added spike collision

---
+++ Assets/Minigames/Platformer/Scripts/SpikeCollision.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeCollision : MonoBehaviour
+{
+    public PlatformerLogic logic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Spikes").GetComponent<PlatformerLogic>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+    }
+}
