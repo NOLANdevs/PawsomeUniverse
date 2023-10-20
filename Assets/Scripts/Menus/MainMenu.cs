5f204727562bf664b22bb310ce987000905f701b
Oliver Huang <oliverwhuang@gmail.com>
2023-10-21 11:20:45 +1300

    Volume slider
    Added volume text to match slider

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private Slider volumeSlider;
+
+    [SerializeField] private TextMeshProUGUI volumeText;
+
+    public void Start()
+    {
+        volumeSlider.onValueChanged.AddListener((v) => volumeText.text = (v).ToString("0%"));
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("Main");
@@ -15,4 +26,6 @@ public class MainMenu : MonoBehaviour
         Debug.Log("Quit");
         Application.Quit();
     }
+
+
 }
