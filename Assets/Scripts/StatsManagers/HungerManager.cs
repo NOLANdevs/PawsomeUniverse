62db431c58e0fbb34e9a128820cf267055f86830
nate <jmy3092@autuni.ac.nz>
2023-10-23 19:31:33 +1300

    Move initialisation back

--- Assets/Scripts/StatsManagers/HungerManager.cs
+++ Assets/Scripts/StatsManagers/HungerManager.cs
@@ -15,13 +15,11 @@ public class HungerManager : MonoBehaviour
 
     void Start()
     {
+        animator = GameObject.FindWithTag("Animated").GetComponent<Animator>();
     }
 
     void Update()
     {
-        // Set animator
-        animator = GameObject.FindWithTag("Animated").GetComponent<Animator>();
-
         // Time since last ate
         timeSinceStartedEating += Time.deltaTime;
         if (timeSinceStartedEating > eatingTime)
